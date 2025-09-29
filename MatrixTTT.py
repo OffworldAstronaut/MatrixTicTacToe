ebd9392e5717b7dac98201133bb6f2fd94846079
OffworldAstronaut <rafael.science.amorim@gmail.com>
2025-09-29 17:37:20 -0300

    feat: translation to english; wip refactoring

--- MatrixTTT.py
+++ MatrixTTT.py
@@ -1,276 +1,248 @@
-# Tipagem especial 
-from typing import Optional, List, Tuple
-# Módulo numpy para as funções relacionadas às matrizes 
-import numpy as np  
-# Função randint do módulo random para necessidades aleatórias 
-from random import randint 
+from typing import Optional, List, Tuple                    # Typing
+import numpy as np                                          # Numerical utilities
+from random import randint                                  # Random integers
 
-class Jogador:
-    """Classe abstrata que engloba os dois tipos possíveis de jogadores: humano e computador"""
-    def __init__(self, simbolo : int) -> None:
-        """Instancia um objeto da classe Jogador 
+class Player:
+    """Abstract class that encompasses the two possible types of players: human and computer"""
+    def __init__(self, symbol: int) -> None:
+        """Instantiates an object of the Player class
 
         Args:
-            simbolo (str): Número utilizado pelo jogador para preencher a matriz, pode ser '0' ou '1' 
+            symbol (str): Number used by the player to fill the matrix. Can be 0 or 1. 
         """
-        self.simbolo = simbolo
+        self.symbol = symbol
 
-class JogadorHumano(Jogador):
-    """Classe que representa um jogador humano, especialização da classe Jogador."""    
-    def __init__(self, simbolo : int): 
-        """Inicializa uma instância da classe
+class HumanPlayer(Player):
+    """Class representing a human player, specialization of the Player class."""
+    def __init__(self, symbol: int):
+        super().__init__(symbol)
+        self.symbol = symbol
 
-        Args:
-            simbolo (str): Símbolo do jogador
-        """        
-        super().__init__(simbolo)
-        self.simbolo = simbolo 
-
-    def fazer_jogada(self) -> Tuple[int, int]: 
-        """Possibilita o jogador humano a fazer uma jogada no tabuleiro 
+    def make_move(self) -> Tuple[int, int]:
+        """Allows the human player to make a move on the board
 
         Returns:
-            pos (Tuple[int, int]): Tupla contendo a posição no tabuleiro que a jogada será feita 
-        """        
-        print(f"Jogador: {self.simbolo}")
-        pergunta_jogada = input("> Posição: ")
+            pos (Tuple[int, int]): Tuple containing the position on the board where the move will be made
+        """
+        # Move interface
+        print(f"Player: {self.symbol}")
+        move_input = input("> Position: ")
 
-        # Seleciona a tupla que representa uma posição no tabuleiro de acordo com a posição escolhida pelo jogador
-        if pergunta_jogada == 'a':
+        # Selects the tuple representing a position on the board according to the player's choice
+        if move_input == 'a':
             pos = (0, 0)
-        elif pergunta_jogada == 'b': 
+        elif move_input == 'b':
             pos = (0, 1)
-        elif pergunta_jogada == 'c': 
+        elif move_input == 'c':
             pos = (0, 2)
-        elif pergunta_jogada == 'd': 
+        elif move_input == 'd':
             pos = (1, 0)
-        elif pergunta_jogada == 'e': 
-            pos = (1, 1) 
-        elif pergunta_jogada == 'f': 
-            pos = (1, 2) 
-        elif pergunta_jogada == 'g': 
-            pos = (2, 0) 
-        elif pergunta_jogada == 'h': 
-            pos = (2, 1) 
-        else: 
+        elif move_input == 'e':
+            pos = (1, 1)
+        elif move_input == 'f':
+            pos = (1, 2)
+        elif move_input == 'g':
+            pos = (2, 0)
+        elif move_input == 'h':
+            pos = (2, 1)
+        else:
             pos = (2, 2)
-    
-        return pos 
-
-class JogadorComputador(Jogador):
-    """Classe que representa um jogador que é um computador, especialização da classe Jogador"""    
-    def __init__(self, simbolo : int, estrategia : str="aleatoria") -> None: 
-        """Inicializa uma instância da classe
-
-        Args:
-            simbolo (str): Símbolo associado ao jogador 
-            estrategia (str): Estratégia escolhida para a máquina
-        """        
-        super().__init__(simbolo)
-        self.simbolo = simbolo 
-        self.estrategias = ["aleatoria", "inteligente", "darksouls"]
-        if estrategia in self.estrategias:
-            self.estrategia = estrategia
-        else: 
-            self.estrategia = "aleatoria"
-
-    def fazer_jogada(self) -> Tuple[int, int]: 
-        """Possibilita o computador a fazer uma jogada no tabuleiro
-
-        Returns:
-            Tuple[int, int]: Tupla contendo a posição no tabuleiro que a jogada será feita 
-        """
-        # Estratégia aleatória 
-        if self.estrategia == "aleatoria":
-            # Escolhe aleatoriamente uma linha 
-            linha_aleatoria = randint(0, 2)
-            # Escolhe aleatoriamente uma coluna 
-            coluna_aleatoria = randint(0, 2)
-            # Gera a tupla "posição"
-            pos = (linha_aleatoria, coluna_aleatoria)
 
         return pos
 
-class Tabuleiro: 
-    """Simula o tabuleiro do jogo"""    
-    # letras disponíveis das casas do tabuleiro 
-    letras_casas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
-    # inicializa um objeto tabuleiro criando uma matriz numpy
-    def __init__(self) -> None: 
-        # Cria primeiro uma lista de números de 0 a 8 e depois tranforma essa lista numa matriz 3 x 3 
-        self.casas = [
+class ComputerPlayer(Player):
+    """Class representing a computer player, specialization of the Player class"""
+    def __init__(self, symbol : int) -> None:
+        super().__init__(symbol)
+        self.symbol = symbol
+
+    def make_move(self) -> Tuple[int, int]:
+        """Allows the computer to make a move on the board
+
+        Returns:
+            Tuple[int, int]: Tuple containing the position on the board where the move will be made
+        """
+        # Randomly choose a row
+        random_row = randint(0, 2)
+        # Randomly choose a column
+        random_col = randint(0, 2)
+        # Generate the "position" tuple
+        pos = (random_row, random_col)
+        return pos
+
+class Board:
+    """Simulates the game board"""
+    # Available letters for the board squares
+    square_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+    def __init__(self) -> None:
+        self.squares = [
             ['a', 'b', 'c'],
-            ['d', 'e', 'f'], 
+            ['d', 'e', 'f'],
             ['g', 'h', 'i']
         ]
 
-    def verificar_casa_vazia(self, pos: Tuple[int, int]) -> bool:  
-        """Verifica se uma dada casa do tabuleiro está vazia
+    def is_square_empty(self, pos: Tuple[int, int]) -> bool:
+        """Checks if a given square on the board is empty
 
         Args:
-            pos (Tuple[int, int]): Tupla contendo a posição do tabuleiro a ser verificada
+            pos (Tuple[int, int]): Tuple containing the board position to be checked
 
         Returns:
-            bool: booleano indicando se a casa está vazia (True) ou não (False)
-        """        
-        if self.pegar_tabuleiro()[pos[0]][pos[1]] not in Tabuleiro.letras_casas: 
+            bool: Boolean indicating if the square is empty (True) or not (False)
+        """
+        if self.get_board()[pos[0]][pos[1]] not in Board.square_letters:
             return False
-        
-        else: 
+        else:
             return True
 
-    def marcar_casa(self, pos : Tuple[int, int], simbolo : str) -> bool: 
-        """Marca uma determinada casa do tabuleiro com o símbolo do jogador
+    def mark_square(self, pos : Tuple[int, int], symbol : str) -> bool:
+        """Marks a specific square on the board with the player's symbol
 
         Args:
-            pos (Tuple[int, int]): Posição a ser marcada 
-            simbolo (str): Símbolo do jogador que está jogando
-        """        
-        # Marca uma casa com o símbolo do jogador. Deve verificar se a casa selecionada está vazia 
-        self.casas[pos[0]][pos[1]] = simbolo
+            pos (Tuple[int, int]): Position to be marked
+            symbol (str): Symbol of the player making the move
+        """
+        # Marks a square with the player's symbol.
+        self.squares[pos[0]][pos[1]] = symbol
 
-    def pegar_tabuleiro(self) -> List[List[str]]:
-        """Retorna o tabuleiro em formato de uma lista de listas, com cada sub-lista sendo uma linha do tabuleiro
+    def get_board(self) -> List[List[str]]:
+        """Returns the board as a list of lists, with each sub-list being a row of the board
 
-        Ex.: 
+        Ex.:
 
         [
-            [1, 0, 1], 
-            [1, 0, 0], 
+            [1, 0, 1],
+            [1, 0, 0],
             [0, 1, 0]
         ]
 
         Returns:
-            lista_tabuleiro (List[List[str]]): Tabuleiro no formato desejado
+            board_list (List[List[str]]): Board in the desired format
         """
-        lista_tabuleiro = self.casas
-        return lista_tabuleiro
+        board_list = self.squares
+        return board_list
 
-    def imprimir_tabuleiro(self) -> None: 
-        """Imprime o tabuleiro na tela para o jogador, de forma organizada"""        
+    def print_board(self) -> None:
+        """Prints the board on the screen for the player, in an organized way"""
         print(
             f"""
 
               |      |
-          {self.pegar_tabuleiro()[0][0]}   |   {self.pegar_tabuleiro()[0][1]}  |   {self.pegar_tabuleiro()[0][2]}
+          {self.get_board()[0][0]}   |   {self.get_board()[0][1]}  |   {self.get_board()[0][2]}
               |      |
         ---------------------
               |      |
-          {self.pegar_tabuleiro()[1][0]}   |   {self.pegar_tabuleiro()[1][1]}  |   {self.pegar_tabuleiro()[1][2]}
+          {self.get_board()[1][0]}   |   {self.get_board()[1][1]}  |   {self.get_board()[1][2]}
               |      |
         ---------------------
               |      |
-           {self.pegar_tabuleiro()[2][0]}  |   {self.pegar_tabuleiro()[2][1]}  |   {self.pegar_tabuleiro()[2][2]}
+           {self.get_board()[2][0]}  |   {self.get_board()[2][1]}  |   {self.get_board()[2][2]}
               |      |
-            
+
             """)
 
-    def verificar_preenchimento(self) -> bool: 
-        """Verifica se o tabuleiro está completamente preenchido, uma condição necessária para determinar se o jogo está empatado
+    def is_board_full(self) -> bool:
+        """Checks if the board is completely filled, a necessary condition to determine if the game is a draw
 
         Returns:
-            bool: Booleano indicando se o tabuleiro está completamente preenchido ou não (True, se sim, False, caso contrário)
-        """        
-        # variáveis booleanas para verificar o preenchimento de cada linha 
-        linha1_preenchida = not self.verificar_casa_vazia((0, 0)) and not self.verificar_casa_vazia((0, 1)) and not self.verificar_casa_vazia((0, 2))
-        linha2_preenchida = not self.verificar_casa_vazia((1, 0)) and not self.verificar_casa_vazia((1, 1)) and not self.verificar_casa_vazia((1, 2))
-        linha3_preenchida = not self.verificar_casa_vazia((2, 0)) and not self.verificar_casa_vazia((2, 1)) and not self.verificar_casa_vazia((2, 2))
+            bool: Boolean indicating if the board is completely filled or not (True if yes, False otherwise)
+        """
+        # Boolean variables to check if each row is filled
+        row1_full = not self.is_square_empty((0, 0)) and not self.is_square_empty((0, 1)) and not self.is_square_empty((0, 2))
+        row2_full = not self.is_square_empty((1, 0)) and not self.is_square_empty((1, 1)) and not self.is_square_empty((1, 2))
+        row3_full = not self.is_square_empty((2, 0)) and not self.is_square_empty((2, 1)) and not self.is_square_empty((2, 2))
 
-        # se o tabuleiro inteiro estiver preenchido (todas as variáveis verdadeiras), marque o tabuleiro como preenchido
-        if linha1_preenchida and linha2_preenchida and linha3_preenchida:
+        # if the entire board is filled (all variables true), mark the board as full
+        if row1_full and row2_full and row3_full:
             return True
-
-        else: 
+        else:
             return False
 
-class MatrixTTT: 
-    """Classe principal. Aplica todas as outras classes para fazer com que o jogo seja executado. 
-    """    
-    def __init__(self, j1 : Jogador, j2 : Jogador) -> None:
-        """Instancia um jogo-da-velha entre dois jogadores 
+class MatrixTTT:
+    """Main class. Applies all other classes to make the game run."""
+    def __init__(self, p1 : Player, p2 : Player) -> None:
+        """Instantiates a tic-tac-toe game between two players
 
         Args:
-            j1 (Jogador): 'Jogador 1', o primeiro jogador
-            j2 (Jogador): 'Jogador 2', o segundo jogador 
-        """        
-        # lista que armazena os dois jogadores  
-        self.jogadores = [j1, j2]
-        # objeto Tabuleiro: o jogo não existe sem o tabuleiro 
-        self.tabuleiro = Tabuleiro() 
-        # variável para marcar de qual turno é a vez: 0 se for o jogador um e 1 se for o jogador 2; -1 é o caso inicial
-        self.turno = -1
+            p1 (Player): 'Player 1', the first player
+            p2 (Player): 'Player 2', the second player
+        """
+        # list that stores the two players
+        self.players = [p1, p2]
+        # Board object: the game does not exist without the board
+        self.board = Board()
+        # variable to mark whose turn it is: 0 if it's player one, 1 if it's player two; -1 is the initial case
+        self.turn = -1
 
-    def jogador_atual(self) -> Jogador:
-        """Retorna o jogador que deverá jogar agora
+    def current_player(self) -> Player:
+        """Returns the player who should play now
 
         Returns:
-            Jogador: Objeto "Jogador", que deverá jogar agora
+            Player: "Player" object, who should play now
         """
-        if self.turno == -1:
-            # Ao começar o jogo, o jogador 1 tem a vez, sempre 
-            self.turno = 0
-            return self.jogadores[0]
+        if self.turn == -1:
+            # At the start of the game, player 1 always goes first
+            self.turn = 0
+            return self.players[0]
+        elif self.turn == 0:
+            # if it was player 1's turn, now it's player 2's turn
+            self.turn = 1
+            return self.players[1]
+        else:
+            # if it was player 2's turn, now it's player 1's turn
+            self.turn = 0
+            return self.players[0]
 
-        elif self.turno == 0: 
-            # se a vez era do jogador 1, a vez agora é do jogador 2 
-            self.turno = 1 
-            return self.jogadores[1]
-        else: 
-            # se a vez era do jogador 2, a vez agora é do jogador 1  
-            self.turno = 0 
-            return self.jogadores[0]
-        
-    def jogar(self) -> None:
-        """Simula o jogo propriamente dito. 
-        """        
-        while True: 
-            # Seleciona o jogador atual 
-            j_atual = self.jogador_atual()
+    def play(self) -> None:
+        """Simulates the actual game."""
+        while True:
+            # Select the current player
+            curr_player = self.current_player()
             while True:
-                # Imprime o tabuleiro na tela 
-                self.tabuleiro.imprimir_tabuleiro()
-                # Pede a jogada ao jogador atual por meio da classe Jogador (fazer_jogada)
-                jogada_atual = j_atual.fazer_jogada()
-                # Marca a jogada no tabuleiro, se a casa escolhida estiver vazia
-                if self.tabuleiro.verificar_casa_vazia(jogada_atual):
-                    self.tabuleiro.marcar_casa(jogada_atual, j_atual.simbolo)
+                # Print the board on the screen
+                self.board.print_board()
+                # Ask the current player for a move using the Player class (make_move)
+                current_move = curr_player.make_move()
+                # Mark the move on the board, if the chosen square is empty
+                if self.board.is_square_empty(current_move):
+                    self.board.mark_square(current_move, curr_player.symbol)
                     break
-                # Caso contrário (casa prenchida), o usuário será notificado e terá que escolher outra jogada 
+                # Otherwise (square filled), the user will be notified and must choose another move
                 else:
-                    print("Essa casa já está preenchida!")
+                    print("This square is already filled!")
                     print()
 
-            # Se as condições de vitória para o jogador 1 estiverem sido satisfeitas, anuncie sua vitória
-            if self.checar_fim_de_jogo() == "j1": 
-                print("FIM DE JOGO!")
-                print(f"VITÓRIA DE {self.jogadores[0].simbolo}!")
+            # If the win conditions for player 1 have been met, announce their victory
+            if self.check_game_end() == "p1":
+                print("GAME OVER!")
+                print(f"VICTORY FOR {self.players[0].symbol}!")
                 print()
-                self.tabuleiro.imprimir_tabuleiro()
+                self.board.print_board()
                 break
 
-            # Se as condições de vitória para o jogador 2 estiverem sido satisfeitas, anuncie sua vitória
-            elif self.checar_fim_de_jogo() == "j2": 
-                print("FIM DE JOGO!")
-                print(f"VITÓRIA DE {self.jogadores[1].simbolo}!")
+            # If the win conditions for player 2 have been met, announce their victory
+            elif self.check_game_end() == "p2":
+                print("GAME OVER!")
+                print(f"VICTORY FOR {self.players[1].symbol}!")
                 print()
-                self.tabuleiro.imprimir_tabuleiro()
+                self.board.print_board()
                 break
 
-    def checar_fim_de_jogo(self) -> Optional[str]:
-        """Checa se o jogo acabou, retornando também o resultado caso verdade. 
+    def check_game_end(self) -> Optional[str]:
+        """Checks if the game is over, also returning the result if true.
 
         Returns:
-            vitoria (Optional[str]): Marca a vitória ou empate do jogo atual: "j1" ou "j2"
+            victory (Optional[str]): Marks the victory or draw of the current game: "p1" or "p2"
         """
-        # Variável para marcar o resultado do jogo, padrão "None"
-        vitoria = None
+        # Variable to mark the result of the game, default "None"
+        victory = None
 
-        if self.tabuleiro.verificar_preenchimento() == True: 
-            tabuleiro_matriz = np.array(self.tabuleiro.pegar_tabuleiro())
-            print(tabuleiro_matriz)
-            if np.linalg.det(tabuleiro_matriz) == 0:
-                vitoria = "j1"
-            else: 
-                vitoria = "j2"
+        if self.board.is_board_full() == True:
+            board_matrix = np.array(self.board.get_board())
+            print(board_matrix)
+            if np.linalg.det(board_matrix) == 0:
+                victory = "p1"
+            else:
+                victory = "p2"
 
-        return vitoria 
+        return victory
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MatrixTicTacToe
+# 💊 MatrixTicTacToe
 Simula um jogo da velha modificado se utilizando de uma matriz 3x3, inspirado no problema A4 (Putnam 2002)
 
 ## Ideias futuras 
